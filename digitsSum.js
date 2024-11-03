1001c338c660866874bde9ebb97c56c5bd6d6c07
Mash700 <007mmariaz@gmail.com>
2024-11-03 23:38:07 +0200

    digitsSum

--- digitsSum.js
+++ digitsSum.js
@@ -1,5 +1,22 @@
-let n=prompt("n=");
-if (0 < n < 10000000){
-    let Lenght = length (n);
-    if (let i=1,i<8, ++i)
+let n = prompt("Enter n:");
+if (n > 0 && n < 10000000) {
+    let length = String(n).length;
+    function sumOfDigits(n) {
+        let sum = 0;
+        let rest = n, remainder;
+
+        while (rest) {
+            remainder = rest % 10;
+            sum += remainder;
+            rest = Math.floor(rest / 10); 
+        }
+
+        return sum;
+    }
+
+    let result = sumOfDigits(Number(n));
+    console.log("Sum of digits:", result);
+
+} else {
+    console.log("Please enter a number between 1 and 9999999.");
 }
