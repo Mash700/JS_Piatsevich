70e5902288d95a6bc5ed6baba763e4b6763a5bb0
Mash700 <007mmariaz@gmail.com>
2024-11-04 23:52:04 +0200

    compareTwoNumbers

---
+++ compareTwoNumbers.js
@@ -0,0 +1,9 @@
+function compareNumbers(number1, number2) {
+    if (number1 > number2) {
+        console.log(">");
+    } else if (number1 < number2) {
+        console.log("<");
+    } else if (number1 === number2) {
+        console.log("=");
+    }
+}
